4ecb27eb7842d3bd28ad9e4cc421828c50bbf5a9
Lars Sørensen <larssorensen98@hotmail.com>
2026-01-14 15:48:03 +0100

    Add SecurityConfig resolver
    Add the SecurityConfig resolver which takes in a SecurityConfig resource and resolves whatever needs to be resolved to be able to reconcile all resources. Now this includes fetching the accessPolicy of the referenced application.

---
+++ internal/resolver/securityconfig_resolver.go
@@ -0,0 +1,55 @@
+package resolver
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kartverket/accesserator/api/v1alpha"
+	"github.com/kartverket/accesserator/internal/state"
+	"github.com/kartverket/skiperator/api/v1alpha1"
+	"github.com/kartverket/skiperator/api/v1alpha1/podtypes"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func ResolveSecurityConfig(ctx context.Context, k8sClient client.Client, securityConfig v1alpha.SecurityConfig) (*state.Scope, error) {
+	tokenXEnabled := false
+	if securityConfig.Spec.Tokenx != nil && securityConfig.Spec.Tokenx.Enabled {
+		tokenXEnabled = true
+	}
+	if !tokenXEnabled {
+		return &state.Scope{
+			SecurityConfig: securityConfig,
+			TokenXConfig: state.TokenXConfig{
+				Enabled: tokenXEnabled,
+			},
+		}, nil
+	}
+
+	var skiperatorApplication v1alpha1.Application
+	if exists := k8sClient.Get(ctx, types.NamespacedName{
+		Name:      securityConfig.Spec.ApplicationRef,
+		Namespace: securityConfig.Namespace,
+	}, &skiperatorApplication); exists != nil {
+		return nil, fmt.Errorf(
+			"failed to fetch Application resource named %s: %w",
+			securityConfig.Spec.ApplicationRef,
+			exists,
+		)
+	}
+
+	var skiperatorAccessPolicy *podtypes.AccessPolicy
+	if skiperatorApplication.Spec.AccessPolicy != nil {
+		skiperatorAccessPolicy = skiperatorApplication.Spec.AccessPolicy
+	} else {
+		skiperatorAccessPolicy = nil
+	}
+
+	return &state.Scope{
+		SecurityConfig: securityConfig,
+		TokenXConfig: state.TokenXConfig{
+			Enabled:      tokenXEnabled,
+			AccessPolicy: skiperatorAccessPolicy,
+		},
+	}, nil
+}
