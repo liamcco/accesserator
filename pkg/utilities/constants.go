ff338a878d74a5b13167f7980e42abd3188003b3
Liam <37960435+liamcco@users.noreply.github.com>
2026-02-06 09:06:09 +0100

    feat: parameterize opa config (#4)
    * feat: build config from securityConfig
    
    * feat: add config fields to OpaConfig in SecurtyConfig
    
    * feat: add bundle URL to scope
    
    * test: update example files with bundleURL
    
    * style: restructure crd
    
    * feat: build configmap from constants
    
    * fix: update variable names
    
    * feat: use constants and security config in webhooks
    
    * chore: remove unused yaml
    
    * fix: ensure quoted string for env vars

--- pkg/utilities/constants.go
+++ pkg/utilities/constants.go
@@ -4,4 +4,7 @@ const (
 	JwkerSecretNameSuffix = "jwker-secret"
 	OpaConfigNameSuffix   = "opa-config"
 	EgressNameSuffix      = "egress"
+	OpaGithubTokenEnvVar  = "GITHUB_TOKEN"
+	OpaPublicKeyEnvVar    = "OPA_PUBLIC_KEY"
+	OpaConfigFileName     = "config.yaml"
 )
