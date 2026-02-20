52b37608efc8ea40839461787d206c2045881580
Liam <37960435+liamcco@users.noreply.github.com>
2026-02-20 14:45:33 +0100

    test: reconcile with opa discovery bundle server

--- internal/controller/securityconfig_controller_opa_test.go
+++ internal/controller/securityconfig_controller_opa_test.go
@@ -1,12 +1,18 @@
 package controller
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
 	"context"
+	"io"
 
 	"github.com/kartverket/accesserator/pkg/utilities"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -93,6 +99,66 @@ var _ = Describe("SecurityConfig Controller", func() {
 				Expect(k8sClient.Delete(ctx, jwker)).To(Succeed())
 			}
 
+			By("Cleanup any created Opa config")
+			opaConfig := &corev1.ConfigMap{}
+			opaConfigKey := types.NamespacedName{
+				Name:      utilities.GetOpaConfigName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			if err := k8sClient.Get(ctx, opaConfigKey, opaConfig); err == nil {
+				Expect(k8sClient.Delete(ctx, opaConfig)).To(Succeed())
+			}
+
+			By("Cleanup any created Opa discovery config")
+			opaDiscoveryConfig := &corev1.ConfigMap{}
+			opaDiscoveryConfigKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryConfigName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			if err := k8sClient.Get(ctx, opaDiscoveryConfigKey, opaDiscoveryConfig); err == nil {
+				Expect(k8sClient.Delete(ctx, opaDiscoveryConfig)).To(Succeed())
+			}
+
+			By("Cleanup any created Opa discovery service")
+			opaDiscoveryService := &corev1.Service{}
+			opaDiscoveryServiceKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryServiceName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			if err := k8sClient.Get(ctx, opaDiscoveryServiceKey, opaDiscoveryService); err == nil {
+				Expect(k8sClient.Delete(ctx, opaDiscoveryService)).To(Succeed())
+			}
+
+			By("Cleanup any created Opa discovery deployment")
+			opaDiscoveryDeployment := &appsv1.Deployment{}
+			opaDiscoveryDeploymentKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryDeploymentName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			if err := k8sClient.Get(ctx, opaDiscoveryDeploymentKey, opaDiscoveryDeployment); err == nil {
+				Expect(k8sClient.Delete(ctx, opaDiscoveryDeployment)).To(Succeed())
+			}
+
+			By("Cleanup any created Opa discovery egress policy")
+			opaDiscoveryEgressPolicy := &networkingv1.NetworkPolicy{}
+			opaDiscoveryEgressPolicyKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryEgressPolicyName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			if err := k8sClient.Get(ctx, opaDiscoveryEgressPolicyKey, opaDiscoveryEgressPolicy); err == nil {
+				Expect(k8sClient.Delete(ctx, opaDiscoveryEgressPolicy)).To(Succeed())
+			}
+
+			By("Cleanup any created Opa discovery ingress policy")
+			opaDiscoveryIngressPolicy := &networkingv1.NetworkPolicy{}
+			opaDiscoveryIngressPolicyKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryIngressPolicyName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			if err := k8sClient.Get(ctx, opaDiscoveryIngressPolicyKey, opaDiscoveryIngressPolicy); err == nil {
+				Expect(k8sClient.Delete(ctx, opaDiscoveryIngressPolicy)).To(Succeed())
+			}
+
 			skiperatorApp := &v1alpha1.Application{}
 			appKey := types.NamespacedName{Name: skiperatorAppName, Namespace: namespaceName}
 			err = k8sClient.Get(ctx, appKey, skiperatorApp)
@@ -121,6 +187,56 @@ var _ = Describe("SecurityConfig Controller", func() {
 			}
 			Eventually(func() error {
 				return k8sClient.Get(ctx, opaConfigKey, &opaConfig)
+			}).Should(Succeed())
+
+			By("Verifying that a Opa discovery ConfigMap was created")
+			var opaDiscoveryConfig corev1.ConfigMap
+			opaDiscoveryConfigKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryConfigName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, opaDiscoveryConfigKey, &opaDiscoveryConfig)
+			}).Should(Succeed())
+
+			By("Verifying that a Opa discovery Service was created")
+			var opaDiscoveryService corev1.Service
+			opaDiscoveryServiceKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryServiceName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, opaDiscoveryServiceKey, &opaDiscoveryService)
+			}).Should(Succeed())
+
+			By("Verifying that a Opa discovery Deployment was created")
+			var opaDiscoveryDeployment appsv1.Deployment
+			opaDiscoveryDeploymentKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryDeploymentName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, opaDiscoveryDeploymentKey, &opaDiscoveryDeployment)
+			}).Should(Succeed())
+
+			By("Verifying that a Opa discovery egress NetworkPolicy was created")
+			var opaDiscoveryEgressPolicy networkingv1.NetworkPolicy
+			opaDiscoveryEgressPolicyKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryEgressPolicyName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, opaDiscoveryEgressPolicyKey, &opaDiscoveryEgressPolicy)
+			}).Should(Succeed())
+
+			By("Verifying that a Opa discovery ingress NetworkPolicy was created")
+			var opaDiscoveryIngressPolicy networkingv1.NetworkPolicy
+			opaDiscoveryIngressPolicyKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryIngressPolicyName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, opaDiscoveryIngressPolicyKey, &opaDiscoveryIngressPolicy)
 			}).Should(Succeed())
 
 			By("Verifying that SecurityConfig is PhasePending before OpaConfig is ready")
@@ -185,6 +301,56 @@ var _ = Describe("SecurityConfig Controller", func() {
 				return errors.IsNotFound(err)
 			}).Should(BeTrue())
 
+			By("Verifying that no Opa discovery ConfigMap resource exists")
+			var opaDiscoveryConfig corev1.ConfigMap
+			Consistently(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      utilities.GetOpaDiscoveryConfigName(skiperatorAppName),
+					Namespace: namespaceName,
+				}, &opaDiscoveryConfig)
+				return errors.IsNotFound(err)
+			}).Should(BeTrue())
+
+			By("Verifying that no Opa discovery Service resource exists")
+			var opaDiscoveryService corev1.Service
+			Consistently(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      utilities.GetOpaDiscoveryServiceName(skiperatorAppName),
+					Namespace: namespaceName,
+				}, &opaDiscoveryService)
+				return errors.IsNotFound(err)
+			}).Should(BeTrue())
+
+			By("Verifying that no Opa discovery Deployment resource exists")
+			var opaDiscoveryDeployment appsv1.Deployment
+			Consistently(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      utilities.GetOpaDiscoveryDeploymentName(skiperatorAppName),
+					Namespace: namespaceName,
+				}, &opaDiscoveryDeployment)
+				return errors.IsNotFound(err)
+			}).Should(BeTrue())
+
+			By("Verifying that no Opa discovery egress NetworkPolicy resource exists")
+			var opaDiscoveryEgressPolicy networkingv1.NetworkPolicy
+			Consistently(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      utilities.GetOpaDiscoveryEgressPolicyName(skiperatorAppName),
+					Namespace: namespaceName,
+				}, &opaDiscoveryEgressPolicy)
+				return errors.IsNotFound(err)
+			}).Should(BeTrue())
+
+			By("Verifying that no Opa discovery ingress NetworkPolicy resource exists")
+			var opaDiscoveryIngressPolicy networkingv1.NetworkPolicy
+			Consistently(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      utilities.GetOpaDiscoveryIngressPolicyName(skiperatorAppName),
+					Namespace: namespaceName,
+				}, &opaDiscoveryIngressPolicy)
+				return errors.IsNotFound(err)
+			}).Should(BeTrue())
+
 			By("Verifying events were emitted")
 			Eventually(fakeRecorder.Events).Should(Receive(ContainSubstring("ReconcileStarted")))
 			Eventually(fakeRecorder.Events).Should(Receive(ContainSubstring("ReconciledSuccessfully")))
@@ -194,5 +360,87 @@ var _ = Describe("SecurityConfig Controller", func() {
 			Eventually(fakeRecorder.Events).ShouldNot(Receive(ContainSubstring("StatusUpdateFailed")))
 			Eventually(fakeRecorder.Events).ShouldNot(Receive(ContainSubstring("ReconcileFailed")))
 		})
+
+		It("should update the Opa discovery document when bundle version changes", func() {
+			fakeRecorder := record.NewFakeRecorder(100)
+			controllerReconciler := getSecurityConfigReconciler(fakeRecorder)
+
+			By("Reconciling once to create the initial Opa discovery resources")
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			opaDiscoveryConfigKey := types.NamespacedName{
+				Name:      utilities.GetOpaDiscoveryConfigName(skiperatorAppName),
+				Namespace: namespaceName,
+			}
+
+			By("Verifying the initial bundle version is present in discovery document")
+			var opaDiscoveryConfig corev1.ConfigMap
+			Eventually(func() error {
+				return k8sClient.Get(ctx, opaDiscoveryConfigKey, &opaDiscoveryConfig)
+			}).Should(Succeed())
+			initialDiscoveryData, initialDiscoveryErr := extractDiscoveryDataJSON(
+				opaDiscoveryConfig.BinaryData[utilities.OpaDiscoveryBundleFileName],
+			)
+			Expect(initialDiscoveryErr).NotTo(HaveOccurred())
+			Expect(initialDiscoveryData).To(
+				ContainSubstring("ghcr.io/kartverket/taaask-poc:latest"),
+			)
+
+			By("Updating bundle version in SecurityConfig")
+			currentSecurityConfig := &accesseratorv1alpha.SecurityConfig{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, currentSecurityConfig)).To(Succeed())
+			currentSecurityConfig.Spec.Opa.BundleVersion = "v2.0.0"
+			Expect(k8sClient.Update(ctx, currentSecurityConfig)).To(Succeed())
+
+			By("Reconciling again to update discovery document")
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Verifying discovery document now contains the updated bundle version")
+			Eventually(func() string {
+				updatedConfig := &corev1.ConfigMap{}
+				if getErr := k8sClient.Get(ctx, opaDiscoveryConfigKey, updatedConfig); getErr != nil {
+					return ""
+				}
+				discoveryDataJSON, discoveryDataErr := extractDiscoveryDataJSON(
+					updatedConfig.BinaryData[utilities.OpaDiscoveryBundleFileName],
+				)
+				if discoveryDataErr != nil {
+					return ""
+				}
+				return discoveryDataJSON
+			}).Should(ContainSubstring("ghcr.io/kartverket/taaask-poc:v2.0.0"))
+		})
 	})
 })
+
+func extractDiscoveryDataJSON(bundle []byte) (string, error) {
+	gzipReader, err := gzip.NewReader(bytes.NewReader(bundle))
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = gzipReader.Close() }()
+
+	tarReader := tar.NewReader(gzipReader)
+	for {
+		header, nextErr := tarReader.Next()
+		if nextErr == io.EOF {
+			return "", io.EOF
+		}
+		if nextErr != nil {
+			return "", nextErr
+		}
+		if header.Name == "data.json" {
+			content, readErr := io.ReadAll(tarReader)
+			if readErr != nil {
+				return "", readErr
+			}
+			return string(content), nil
+		}
+	}
+}
